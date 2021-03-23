dbcbd36be1261d96ccaf943bfafc3b97b8ebc2af
M Carlise <micarlise@gmail.com>
2021-03-22 22:05:41 -0400

    routes/followers: minimal follow, unfollow, getFollwers

--- test/http.test.js
+++ test/http.test.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const photoRouter = require('../routes/photos');
 const userRouter = require('../routes/users');
+const followRouter = require('../routes/followers');
 
 const app = express();
 
 app.use('/photos', photoRouter);
 app.use('/users', userRouter);
+app.use('/followers', followRouter);
 
 describe('/users', function() {
 
@@ -24,6 +26,13 @@ describe('/users', function() {
             .expect(200, done);
     });
 
+    it('POST / 200', function(done) {
+        request(app)
+            .post('/users')
+            .send({username: 'otheruser', email: 'other@user.com'})
+            .expect(200, done);
+    });
+
     it('GET / 200', function(done) {
         request(app)
             .get('/users/testuser')
@@ -31,9 +40,20 @@ describe('/users', function() {
     });
 });
 
+describe('/followers', function() {
+
+    it('GET /followers 200', function(done) {
+
+        request(app)
+            .get('/followers/testuser/follows/otheruser')
+            .expect(200, done);
+    });
+});
+
 describe('/photos', function() {
 
     it('GET / 404', function(done) {
+
         request(app)
             .get('/photos/testuser')
             .expect(404, done);
